c857f1df29471015d9bf54e810227df0a9c688c0
Gabkoi <gabakoi18@gmail.com>
2023-12-22 02:37:05 +0000

    this is updated code

--- Agri-Trade/App.js
+++ Agri-Trade/App.js
@@ -1,4 +1,4 @@
-// Navigation
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -8,88 +8,36 @@ import { AntDesign } from '@expo/vector-icons';
 import HomeScreen from './Screens/HomeScreen';
 import LoginScreen from './Screens/Login';
 import SignupScreen from './Screens/SignUp';
-// import { KeyboardAvoidingView } from 'react-native';
-
-
+import AddToCardScreen from './Screens/AddToCard';
+import UserProfileScreen from './Screens/UserProfile';
 
 const HomeStack = createNativeStackNavigator();
 const LoginStack = createNativeStackNavigator();
 const SignupStack = createNativeStackNavigator();
-
-
-
-// This is the function that
+const AddToCardStack = createNativeStackNavigator();
+const UserProfileStack =  createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
-
-// this is going to contain all of the function
-
-// this contain the Home
-const HomeStackScreen = () => {
-  return (
-    <HomeStack.Navigator>
-      <HomeStack.Screen
-        name='HomeStack'
-        component={HomeScreen}
-        options={{
-          title: "Homepage",
-          headerShown: false
-        }}
-      />
-    </HomeStack.Navigator>
-  );
-};
-
-
-// This contain the  Login
-const LoginStackScreen = () => {
-  return (
-    <LoginStack.Navigator>
-      <LoginStack.Screen
-        name='LoginTack'
-        component={LoginScreen}
-        options={{
-          title: "login",
-          headerShown: false,
-          tabBarVisible: false,
-        }}
-      />
-    </LoginStack.Navigator>
-  );
-};
-
-// this contain the Signup
-const SignupStackScreen = () => (
-  <SignupStack.Navigator>
-    <SignupStack.Screen
-      name='SignupStack'
-      component={SignupScreen} 
-      options={{
-        title: "Sign Up",
-        headerShown: false
-      }}
-    />
-  </SignupStack.Navigator>
-);
 
 
 const App = () => (
   <NavigationContainer>
     <Tab.Navigator
-        screenOptions={{
-          tabBarStyle: {
-            backgroundColor: '#ffff',
-            borderTopRightRadius: 25,
-            borderTopLeftRadius: 25,
-            height: 70,
-          },
-        }}
-        initialRouteName='SignIn'
-      >
+      initialRouteName='SignIn'
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: '#ffff',
+          borderTopRightRadius: 25,
+          borderTopLeftRadius: 25,
+          height: 70,
+        },
+      }}
+    >
+
       <Tab.Screen
         name="Homepage"
-        component={HomeStackScreen} 
+        component={HomeStackScreen}
         options={{
-          tabBarIcon:() => <AntDesign name="home" size={30} color="black" />,
+          tabBarIcon: () => <AntDesign name="home" size={30} color="black" />,
           headerShown: false,
           tabBarLabel: '',
           title: "Create New Task",
@@ -98,32 +46,138 @@ const App = () => (
       />
 
       <Tab.Screen
-          name="SignIn"
-          component={LoginScreen}
-          options={{
-            tabBarIcon: () => <AntDesign name="user" size={35} color="#fff" />,
-            tabBarLabel: '',
-            title: "Sign In",
-            headerTitleAlign: "center",
-            // headerShown: false,
-            tabBarStyle: {
-              display: 'none',
-             },
-          }}
-        />
-
+        name="SignIn"
+        component={LoginStackScreen}
+        options={{
+          tabBarIcon: () => <AntDesign name="user" size={35} color="#fff" />,
+          tabBarLabel: '',
+          title: "Sign In",
+          headerTitleAlign: "center",
+          headerShown: false,
+          tabBarVisible: false,
+          tabBarStyle: {
+            display: 'none',
+          },
+        }}
+      />
+     {
       <Tab.Screen
         name="SignUp"
-        component={SignupStackScreen} 
+        component={SignupStackScreen}
         options={{
-          tabBarIcon: () => <AntDesign name="user" size={30} color="black" />,  tabBarLabel: '',
-            title: "Create New Task",
-            headerTitleAlign: "center",
-            headerShown: false
+          tabBarIcon: () => <AntDesign name="user" size={30} color="black" />,
+          tabBarLabel: '',
+          title: "Sign Up",
+          headerTitleAlign: "center",
+          headerShown: false,
+        }}
+      /> }
+
+      <Tab.Screen
+        name="AddToCard"
+        component={AddToCardScreen}
+        options={{
+          tabBarIcon: () => <AntDesign name="shoppingcart" size={24} color="black" />,
+          tabBarLabel: '',
+          title: "Add to Card",
+          tabBarVisible: false,
+          // tabBarStyle: {
+          //   display: 'none',
+          // },
+        }}
+      />
+      
+      <Tab.Screen
+        name="Profile"
+        component={AddToCardScreen}
+        options={{
+          tabBarIcon: () => <AntDesign name="user" size={30} color="black" />,
+          tabBarLabel: '',
+          title: "User profile",
+          tabBarVisible: false,
+          // tabBarStyle: {
+          //   display: 'none',
+          // },
         }}
       />
     </Tab.Navigator>
   </NavigationContainer>
 );
 
-export default App
+
+
+// Home Stack Screen
+const HomeStackScreen = () => (
+  <HomeStack.Navigator>
+    <HomeStack.Screen
+      name='HomeStack'
+      component={HomeScreen}
+      options={{
+        title: "Homepage",
+        headerShown: false
+      }}
+    />
+  </HomeStack.Navigator>
+);
+
+// Login Stack Screen
+const LoginStackScreen = () => (
+  <LoginStack.Navigator>
+    <LoginStack.Screen
+      name='LoginTack'
+      component={LoginScreen}
+      options={{
+        title: "Login",
+        headerShown: false,
+        tabBarVisible: false,
+      }}
+    />
+  </LoginStack.Navigator>
+);
+
+// Signup Stack Screen
+const SignupStackScreen = () => (
+  <SignupStack.Navigator>
+    <SignupStack.Screen
+      name='SignupStack'
+      component={SignupScreen}
+      options={{
+        title: "Sign Up",
+        headerShown: false
+      }}
+    />
+  </SignupStack.Navigator>
+);
+
+const AddToCardStackScreen = () => (
+  <AddToCardStack.Navigator>
+    <AddToCardStack.Screen
+      name='add to card '
+      component={AddToCardScreen}
+      options={{
+        title: "Add to card",
+        headerShown: false
+      }}
+    />
+  </AddToCardStack.Navigator>
+);
+
+
+const UserProfileStackScreen = () => (
+  <UserProfileStack.Navigator>
+    <UserProfile.Screen
+     name='Profile'
+     component={UserProfileScreen}
+     options={{
+       title: "Profile",
+       headerShown: false
+     }}
+    />
+    
+  </UserProfileStack.Navigator>
+
+
+
+)
+
+export default App;
---
+++ Agri-Trade/Screens/AddToCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { View, Text, StyleSheet} from "react-native";
+
+
+const AddToCard = () =>{
+    return(
+        <View style={styles.try}>
+            <Text>This is the add to card</Text>
+        </View>
+    )
+}
+
+export default AddToCard
+
+
+
+const styles = StyleSheet.create({
+
+    try:{
+        marginTop:20
+    }
+
+})
